98847fa84c54f1da44a78d838961bd3f5e7d6660
Cameron Aragon <caragon4695@gmail.com>
2020-10-08 19:04:10 -0600

    Add Level 4

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,45 @@
-function nameMenuItem() {
-
+function nameMenuItem(menuItemName) {
+  var result = `Delicious ${menuItemName}`;
+  return result;
 }
 
+function createMenuItem(menuItemName, menuItemPrice, menuItemType) {
+  var menuItem = {
+    name: menuItemName,
+    price: menuItemPrice,
+    type: menuItemType,
+  };
+  return menuItem;
+}
 
+function addIngredients(ingredient, ingredients) {
+  if (ingredients.includes(ingredient)) return;
+  ingredients.push(ingredient);
+};
+
+function formatPrice(initialPrice) {
+  var formattedPrice = `$${initialPrice}`;
+  return formattedPrice;
+};
+
+function decreasePrice(initialPrice) {
+  var decreasedPrice = (initialPrice * .9);
+  return decreasedPrice;
+};
+
+function createRecipe(title, ingredients, menuItemType) {
+  var recipe = {
+    title: title,
+    ingredients: ingredients,
+    type: menuItemType
+  };
+  return recipe;
+}
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe,
 }
-
-
